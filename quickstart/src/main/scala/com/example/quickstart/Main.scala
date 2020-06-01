a774da21149c7cd967a698e18bbb6f6f945b0a39
Sangeetha V S <saransang97@gmail.com>
2020-06-01 15:20:56 +0530

    modified account apis

--- quickstart/src/main/scala/com/example/quickstart/Main.scala
+++
@@ -1,9 +0,0 @@
-//package com.example.quickstart
-//
-//import cats.effect.{ExitCode, IO, IOApp}
-//import cats.implicits._
-//
-//object Main extends IOApp {
-//  def run(args: List[String]) =
-//    QuickstartServer.stream[IO].compile.drain.as(ExitCode.Success)
-//}
